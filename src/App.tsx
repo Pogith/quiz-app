8af3911b69e4bd8728fb47187c9ea861d19d29bc
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-15 18:35:24 +0900

    Feat: add react router at App component and add Routes based on paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+
 function App() {
-  return <>Quiz</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/questions" element={<div>Question Page</div>} />
+          <Route path="/result" element={<div>Result Page</div>} />
+          <Route path="/*" element={<div>Not Found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
