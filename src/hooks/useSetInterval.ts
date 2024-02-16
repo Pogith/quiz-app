975fa0c6838ffb087852c9a0e6a3019a3aff9e4f
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-16 23:31:39 +0900

    Feat: add useSetInterval hook

---
+++ src/hooks/useSetInterval.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export const useSetInterval = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((seconds) => seconds + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return seconds;
+};
