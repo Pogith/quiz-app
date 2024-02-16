20613bf7ace7e2145a4537e6f008cf7f82e94404
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-16 17:25:19 +0900

    Feat: add BrowserRouter and redux Provider store at main.tsx and add body style

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import App from "./App.tsx";
+import { store } from "./store/store.ts";
+
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
