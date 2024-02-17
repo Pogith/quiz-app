975263753983a0f12decc5bc9bade1c187ef6b90
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-17 16:00:17 +0900

    Style: add Button component size and color props and add styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,17 +2,28 @@ import styles from "./styles.module.scss";
 
 type Props = {
   text?: string;
+  size?: "small" | "medium" | "large";
+  color?: "primary" | "secondary" | "tertiary";
   onClick?: () => void;
 };
 
-function Button({ text = "Next", onClick }: Props) {
+function Button({
+  text = "Next",
+  size = "small",
+  color = "primary",
+  onClick,
+}: Props) {
   const handleClick = () => {
     onClick && onClick();
   };
 
+  const buttonClassname = `${styles["button"]} ${styles[`${size}`]} ${
+    styles[`${color}`]
+  }`;
+
   return (
     <div className={styles["button-wrapper"]}>
-      <div className={styles["button"]} onClick={handleClick}>
+      <div className={buttonClassname} onClick={handleClick}>
         {text}
       </div>
     </div>
