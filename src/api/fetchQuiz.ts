7ac97014dd119db569036586bfcd98fbfa3dcdcb
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-22 16:51:55 +0900

    Refactor: remove mock response at fetchQuiz to use real server fetching

--- src/api/fetchQuiz.ts
+++ src/api/fetchQuiz.ts
@@ -1,18 +1,6 @@
-import { QuizResponse } from "@/store/quiz/types";
-import { mockData } from "./mock";
-
-const fetchMockQuiz = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(mockData);
-    }, 1000);
-  });
-};
-
 export const fetchQuiz = async () => {
-  // const response = await fetch(import.meta.env.VITE_QUIZ_API_URL);
-  // const data = await response.json();
-  const response = (await fetchMockQuiz()) as Promise<QuizResponse>;
+  const response = await fetch(import.meta.env.VITE_QUIZ_API_URL);
+  const data = await response.json();
 
-  return response;
+  return data;
 };
