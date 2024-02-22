edd5dd726bec5195c20a76715cceb5e247ad07a6
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-22 16:53:49 +0900

    Refactor: add NotFound component at App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
 import Quiz from "./pages/Quiz/Quiz";
 import Result from "./pages/Result/Result";
 import IncorrectAnswerNote from "./pages/IncorrectAnswerNote/IncorrectAnswerNote";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
@@ -15,7 +17,7 @@ function App() {
           path="/incorrect-answer-note"
           element={<IncorrectAnswerNote />}
         />
-        <Route path="/*" element={<div>Not Found</div>} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
   );
