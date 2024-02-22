cbc118fba4fbf6e25572ce914b52f07fc3c06d6f
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-22 16:15:29 +0900

    Style: add Button and modify style at EmptyQuizScreen

--- src/components/EmptyQuizScreen/EmptyQuizScreen.tsx
+++ src/components/EmptyQuizScreen/EmptyQuizScreen.tsx
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.scss";
+import Button from "../Button/Button";
 
 function EmptyQuizScreen() {
-  return <div className={styles["empty-screen"]}>There is No Quiz!</div>;
+  const navigate = useNavigate();
+
+  const handleNavigateHomePage = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["empty-screen"]}>There is No Quiz!</div>
+      <Button text="Home" size="medium" onClick={handleNavigateHomePage} />
+    </div>
+  );
 }
 
 export default EmptyQuizScreen;
