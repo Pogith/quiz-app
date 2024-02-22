042a6d7c770951796f32c2d093f3c0010c184fd0
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-22 16:05:52 +0900

    Style: add Loading component and style

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+function Loading() {
+  return (
+    <div>
+      <div className={styles["loading"]} />
+    </div>
+  );
+}
+
+export default Loading;
