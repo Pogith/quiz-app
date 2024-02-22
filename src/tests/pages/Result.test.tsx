1cececc010cd00a606d564c94fc410ce1a6b941a
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-22 21:35:26 +0900

    Test: modify Result page rendering test by using redux state

--- src/tests/pages/Result.test.tsx
+++ src/tests/pages/Result.test.tsx
@@ -1,18 +1,42 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { screen } from "@testing-library/react";
+import { useSelector } from "react-redux";
 
 import { render } from "../utils";
 
 import Result from "@/pages/Result/Result";
 
+vi.mock("react-redux", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("react-redux")>();
+  return {
+    ...actual,
+    useSelector: vi.fn(),
+  };
+});
+
 describe("Result Page Test", () => {
   it("should render Result Page", () => {
+    const mockQuizResults = {
+      correctAnswersCounts: 2,
+      incorrectAnswersCounts: 4,
+      quizTime: 8,
+    };
+
+    vi.mocked(useSelector).mockReturnValue(mockQuizResults);
+
     render(<Result />);
 
     expect(screen.getByText(/Result!/i)).toBeInTheDocument();
+
     expect(screen.getByText(/Correct Answer/i)).toBeInTheDocument();
+    expect(screen.getByText(/2/i)).toBeInTheDocument();
+
     expect(screen.getByText(/Wrong Answer/i)).toBeInTheDocument();
+    expect(screen.getByText(/4/i)).toBeInTheDocument();
+
     expect(screen.getByText(/Quiz time/i)).toBeInTheDocument();
+    expect(screen.getByText(/00:00:08/i)).toBeInTheDocument();
+
     expect(screen.getByText(/Check My Note/i)).toBeInTheDocument();
     expect(screen.getByText(/Back to Home/i)).toBeInTheDocument();
   });
