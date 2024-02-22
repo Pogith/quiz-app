1bde74130d8d4cc9a7eeb9754c363dcaba2b1257
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-22 20:31:23 +0900

    Test: test navigate routers at App

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -5,6 +5,30 @@ import { render } from "./utils";
 import App from "@/App";
 
 describe("App Test", () => {
+  it("should render and navigate Home Page", () => {
+    render(<App />, { route: "/" });
+
+    expect(location.pathname).eq("/");
+  });
+
+  it("should render and navigate Quiz Page", () => {
+    render(<App />, { route: "/quiz" });
+
+    expect(location.pathname).eq("/quiz");
+  });
+
+  it("should render and navigate Result Page", () => {
+    render(<App />, { route: "/result" });
+
+    expect(location.pathname).eq("/result");
+  });
+
+  it("should render and navigate IncorrectAnswerNote Page", () => {
+    render(<App />, { route: "/incorrect-answer-note" });
+
+    expect(location.pathname).eq("/incorrect-answer-note");
+  });
+
   it("should render 404 Not Found page when routes wrong path", () => {
     render(<App />, { route: "/something-does-not-match" });
 
