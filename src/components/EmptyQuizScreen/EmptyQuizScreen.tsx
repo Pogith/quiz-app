3b163f89c001222455cfcec71ea604ba4e283942
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-22 16:06:14 +0900

    Style: add EmptyQuizScreen component and style

---
+++ src/components/EmptyQuizScreen/EmptyQuizScreen.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.scss";
+
+function EmptyQuizScreen() {
+  return <div className={styles["empty-screen"]}>There is No Quiz!</div>;
+}
+
+export default EmptyQuizScreen;
