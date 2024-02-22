7c1cab607969e7e8af2bb4d9ae0287aab868d714
chlugsdh58 <chlgusdh58@gmail.com>
2024-02-22 17:16:45 +0900

    Feat: add navigate route at Error component

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,10 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 import styles from "./styles.module.scss";
+
+import Button from "../Button/Button";
 
 type Props = {
   error: any;
 };
 
 function Error({ error }: Props) {
+  const navigate = useNavigate();
+
+  const handleNavigateHomePage = () => {
+    navigate("/");
+  };
+
   let errorMessage = <></>;
 
   if (error && error.error["response_code"]) {
@@ -15,6 +25,7 @@ function Error({ error }: Props) {
   return (
     <div className={styles["container"]}>
       <div className={styles["error-screen"]}>{errorMessage}</div>
+      <Button text="Home" size="medium" onClick={handleNavigateHomePage} />
     </div>
   );
 }
